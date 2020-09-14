aff60daabaee33817fa0db436dd121c9f6627d31
BitFreak <blw_matt@rocketmail.com>
2020-09-14 14:21:18 +0300

    added starting code

---
+++ cmake-build-debug/CMakeFiles/webserver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserver.dir/main.cpp.obj"
+  "webserver.exe"
+  "webserver.exe.manifest"
+  "webserver.lib"
+  "webserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
